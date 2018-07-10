bbfd7a832451871e21ec9e9630fef7f40e491b2c
Antonio Brandão <antonioobfilho@gmail.com>
2018-07-09 21:04:22 -0300

    Obtendo os dados do login

---
+++ src/models/credencias.dto.ts
@@ -0,0 +1,4 @@
+export interface CredenciasDTO {
+    email : String;
+    senha : String;
+}
--- src/services/domain/categoria.service.ts
+++ src/services/domain/categoria.service.ts
@@ -10,6 +10,6 @@ export class CategoriaService {
     constructor(public http : HttpClient){    }
 
     findAll() : Observable<CategoriaDTO[]>{
-        return this.http.get<CategoriaDTO[]>(`${API_CONFIG.urlBase}/categoriass`);
+        return this.http.get<CategoriaDTO[]>(`${API_CONFIG.urlBase}/categorias`);
     }
 }
