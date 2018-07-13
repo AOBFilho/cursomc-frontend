3c634a5fa04de72b260c2c024c012d30896f19a1
Antonio Brandão <antonioobfilho@gmail.com>
2018-07-12 21:47:50 -0300

    Pegando email do toke e salvando

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -4,9 +4,12 @@ import { CredenciasDTO } from "../models/credencias.dto";
 import { API_CONFIG } from "../config/api.config";
 import { StorageService } from "./storage.service";
 import { LocalUser } from "../models/local_user";
+import { JwtHelper } from "angular2-jwt";
 
 @Injectable()
 export class AuthService {
+
+    jwtHelper : JwtHelper = new JwtHelper();
 
     constructor(public http : HttpClient, 
                 public storage : StorageService ){
@@ -23,7 +26,8 @@ export class AuthService {
     sucessFulLogin(authorizationValue : string) {
         let tok : string = authorizationValue.substring(7);
         let usr : LocalUser = {
-            token : tok
+            token : tok,
+            email : this.jwtHelper.decodeToken(tok).sub
         }
         this.storage.setLocalUser(usr);
     }
