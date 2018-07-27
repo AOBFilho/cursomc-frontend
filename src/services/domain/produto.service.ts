b8afe4fc5b3fb7f8a78c990790dc6a9ad58654fc
Antonio Brandão <antonioobfilho@gmail.com>
2018-07-27 20:45:46 -0300

    Carregando detalhes do produto

--- src/services/domain/produto.service.ts
+++ src/services/domain/produto.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { API_CONFIG } from "../../config/api.config";
+import { ProdutoDTO } from "../../models/produto.dto";
 
 @Injectable()
 export class ProdutoService {
@@ -9,5 +10,9 @@ export class ProdutoService {
 
     findByCategoria(categoria_id:string){
         return this.http.get(`${API_CONFIG.urlBase}/produtos?categorias=${categoria_id}`);
+    }
+
+    findById(produto_id:string){
+        return this.http.get<ProdutoDTO>(`${API_CONFIG.urlBase}/produtos/${produto_id}`);
     }
 }
