6c0592dc0e898da6890bbc52121fe609d67650f0
Antonio Brandão <antonioobfilho@gmail.com>
2018-08-13 21:04:03 -0300

    tirando foto

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -3,6 +3,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { StorageService } from '../../services/storage.service';
 import { ClienteDTO } from '../../models/cliente.dto';
 import { ClienteService } from '../../services/domain/cliente.service';
+import { CameraOptions, Camera } from '@ionic-native/camera';
 
 
 @IonicPage()
@@ -13,13 +14,15 @@ import { ClienteService } from '../../services/domain/cliente.service';
 export class ProfilePage {
 
   cliente : ClienteDTO;
+  picture : string;
+  cameraOn : boolean = false;
 
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
     public storage : StorageService,
-    public clienteService : ClienteService) {
-  }
+    public clienteService : ClienteService,
+    public camera: Camera) {}
 
   ionViewDidLoad() {
     let localUser = this.storage.getLocalUser();
@@ -36,6 +39,24 @@ export class ProfilePage {
 
   goHomePage(){
     this.navCtrl.setRoot('HomePage');
+  }
+
+  getCamera(){
+    this.cameraOn = true;
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      encodingType: this.camera.EncodingType.PNG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+    
+    this.camera.getPicture(options)
+    .then((imageData) => {
+      this.cameraOn = false;
+      this.picture = 'data:image/png;base64,' + imageData;
+    }, (err) => {
+      this.cameraOn = false; 
+    });
   }
 
 }
