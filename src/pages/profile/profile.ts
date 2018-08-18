65f8da286f5bc63479878ec8d4a3eced6bc3d1fe
Antonio Brandão <antonioobfilho@gmail.com>
2018-08-17 21:21:14 -0300

    get image off gallery

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -45,7 +45,26 @@ export class ProfilePage {
     this.cameraOn = true;
     const options: CameraOptions = {
       quality: 100,
-      destinationType: this.camera.DestinationType.FILE_URI,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.PNG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+    
+    this.camera.getPicture(options)
+    .then((imageData) => {
+      this.cameraOn = false;
+      this.picture = 'data:image/png;base64,' + imageData;
+    }, (err) => {
+      this.cameraOn = false; 
+    });
+  }
+
+  getGallery(){
+    this.cameraOn = true;
+    const options: CameraOptions = {
+      quality: 100,
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+      destinationType: this.camera.DestinationType.DATA_URL,
       encodingType: this.camera.EncodingType.PNG,
       mediaType: this.camera.MediaType.PICTURE
     }
